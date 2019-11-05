7213b411d84ac5f6b64e7af252db519690855a41
Petrson1999 <Petrson1999@gmail.com>
2019-11-05 20:58:58 +0200

    add DataSourceFactory

---
+++ src/main/java/com/vladimirpetrenkodev/railvayticketiffice/persistance/DataSourceFactory.java
@@ -0,0 +1,56 @@
+package com.vladimirpetrenkodev.railvayticketiffice.persistance;
+
+import org.apache.log4j.Logger;
+import org.postgresql.ds.PGPoolingDataSource;
+
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Properties;
+
+import static com.vladimirpetrenkodev.railvayticketiffice.constant.AppConstants.*;
+
+public class DataSourceFactory {
+
+    private static final Logger LOG = Logger.getLogger(DataSourceFactory.class);
+
+    private static final DataSourceFactory INSTANCE = new DataSourceFactory();
+
+    private static DataSource dataSource;
+
+    private DataSourceFactory() {
+    }
+
+    static {
+        Properties properties = new Properties();
+        try {
+            properties.load(DataSourceFactory.class.getResourceAsStream(DB_PROPERTIES));
+            PGPoolingDataSource pgDataSource = new PGPoolingDataSource();
+            pgDataSource.setServerName(properties.getProperty(DB_HOST));
+            pgDataSource.setDatabaseName(properties.getProperty(DB_NAME));
+            pgDataSource.setUser(properties.getProperty(DB_USERNAME));
+            pgDataSource.setPassword(properties.getProperty(DB_PASSWORD));
+            dataSource = pgDataSource;
+            LOG.info("DataSource created: " + dataSource);
+        } catch (IOException e) {
+            LOG.error("Error while reading properties from file!", e);
+        }
+    }
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            connection = dataSource.getConnection();
+        } catch (SQLException e) {
+            LOG.error("Error while connection creation", e);
+        }
+        return connection;
+    }
+
+    public static PreparedStatement getPreparedStatement(String query) throws SQLException {
+        return getConnection().prepareStatement(query);
+    }
+
+}
