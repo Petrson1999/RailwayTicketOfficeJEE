a1afe6d65abea531507169d05ee1017a47ba54d7
Petrson1999 <Petrson1999@gmail.com>
2019-11-18 01:58:20 +0200

    added dispatcher servlet

--- src/main/java/com/vladimirpetrenkodev/railvayticketiffice/constant/PageUrlConstants.java
+++ src/main/java/com/vladimirpetrenkodev/railvayticketiffice/constant/PageUrlConstants.java
@@ -3,9 +3,9 @@ package com.vladimirpetrenkodev.railvayticketiffice.constant;
 public class PageUrlConstants {
 
     public static final String LANDING_PAGE = "/built/templates/index.jsp";
-    public static final String PROFILE_PAGE = "/built/templates/user.jsp";
-    public static final String TICKETS_PAGE = "/built/templates/user.jsp";
-    public static final String ADMIN_PAGE = "/built/templates/user.jsp";
+    public static final String PROFILE_PAGE = "/built/templates/profile.jsp";
+    public static final String TICKETS_PAGE = "/built/templates/tickets.jsp";
+    public static final String ADMIN_PAGE = "/built/templates/admin.jsp";
     public static final String NOT_FOUND_PAGE = "/built/templates/404.jsp";
     public static final String FORBIDDEN_PAGE = "/built/templates/403.jsp";
 
