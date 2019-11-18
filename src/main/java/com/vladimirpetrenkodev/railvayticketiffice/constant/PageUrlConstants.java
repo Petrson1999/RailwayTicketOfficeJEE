30ba611cd620a731f69d3c1eca7eb6b7a00c9fb2
Petrson1999 <Petrson1999@gmail.com>
2019-11-18 03:36:22 +0200

    added page-path constants

--- src/main/java/com/vladimirpetrenkodev/railvayticketiffice/constant/PageUrlConstants.java
+++ src/main/java/com/vladimirpetrenkodev/railvayticketiffice/constant/PageUrlConstants.java
@@ -2,11 +2,11 @@ package com.vladimirpetrenkodev.railvayticketiffice.constant;
 
 public class PageUrlConstants {
 
-    public static final String LANDING_PAGE = "/built/templates/index.jsp";
-    public static final String PROFILE_PAGE = "/built/templates/profile.jsp";
-    public static final String TICKETS_PAGE = "/built/templates/tickets.jsp";
-    public static final String ADMIN_PAGE = "/built/templates/admin.jsp";
-    public static final String NOT_FOUND_PAGE = "/built/templates/404.jsp";
-    public static final String FORBIDDEN_PAGE = "/built/templates/403.jsp";
+    public static final String LANDING_PAGE = "/built/templates/pages/index.jsp";
+    public static final String PROFILE_PAGE = "/built/templates/pages/profile.jsp";
+    public static final String TICKETS_PAGE = "/built/templates/pages/tickets.jsp";
+    public static final String ADMIN_PAGE = "/built/templates/pages/admin.jsp";
+    public static final String NOT_FOUND_PAGE = "/built/templates/pages/404.jsp";
+    public static final String FORBIDDEN_PAGE = "/built/templates/pages/403.jsp";
 
 }
